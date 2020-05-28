cdc04f2d8c4928c4ed832d81e40a58cf92fb7058
Mike Holm <mike.holm@percipient.ai>
2020-05-28 16:27:13 -0600

    add lodash and controlled/uncontrolled example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
-import One from "./One";
-import Two from "./Two";
+import React, { useState } from "react";
+import { get } from "lodash";
 
 import "./App.css";
 
+const MyInput = ({ value, defaultValue, onChange }) => {
+  const [myValue, setMyValue] = useState(value != null ? value : defaultValue);
+  const length = get(myValue, "length");
+
+  return (
+    <div>
+      <input
+        value={myValue}
+        onChange={(e) => {
+          setMyValue(e.target.value);
+          if (onChange) onChange(e);
+        }}
+      />
+      <span>{length}</span>
+    </div>
+  );
+};
+
+const MyContainer = () => {
+  const [value, setValue] = useState("");
+  const [val2, setVal2] = useState("");
+  return (
+    <>
+      <pre>value: {JSON.stringify(value)}</pre>
+      <MyInput value={value} onChange={(e) => setValue(e.target.value)} />
+      <pre>val2: {JSON.stringify(val2)}</pre>
+      <MyInput defaultValue="hello" onChange={(e) => setVal2(e.target.value)} />
+    </>
+  );
+};
+
 const App = () => (
   <div className="App">
-    <h1>Open the console to see output</h1>
-    <One />
-    <Two />
+    <MyContainer />
   </div>
 );
 
